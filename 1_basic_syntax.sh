edefc7512daa4bb73f570b1825644d1ae5af15fd
chaserChoi <chlwogur2010@gmail.com>
2025-05-20 17:01:42 +0900

    05-20 linux2

--- 1_basic_syntax.sh
+++ 1_basic_syntax.sh
@@ -38,3 +38,66 @@ cd
 # 상대 경로 & 절대 경로
 # 상대 경로: cd ../../
 # 절대 경로: cd/home/jaehyeok_choi
+
+# 비어있는 파일 만들기
+touch first_file.txt
+
+# 파일 내용 조회하기
+cat first_file.txt # 터미널 창 출력하기
+nano first_file.txt # nano 편집기 열기
+
+# 파일 내용 상위 n줄 출력하기
+head -n 파일명
+
+# 파일 내용 하위 n줄 출력하기
+# 많이 쓰임 - 최신 로그
+tail -n 파일명
+
+# 파일 내용 실시간 조회
+tail -f 파일명
+
+# 파일 내용 편집기: nano, vi
+# 도스 키 위/아래 통해 이전 명령어 조회
+# 이전에 실행했던 명령어 조회
+history 
+
+# 현재 내 터미널창에 보이는 명령어 내역 정리
+clear
+
+# ^c 통해 입력 명령어 취소
+
+# 파일 삭제 -f 옵션 통해 묻지않고 삭제 
+rm 파일명
+# 폴더 삭제
+rm -r 폴더명
+
+# 터미널창에 문자열("" 붙일 것) 출력
+echo "hello world"
+# echo 통해 파일에 내용 입력
+# > : 덮어쓰기 & >> : 이어쓰기
+echo "hello world" > first_file.txt
+
+# cp = 복사 명령어
+# cp 복사대상 복사파일명
+# 폴더 복사시에는 -r 옵션 
+cp 1_file.txt 2_file.txt
+cp 1_file.txt ../2_file.txt # 상위 폴더로 복사
+cp -r abc ../abc
+
+# grep : 파일 내에서 문자열 찾기
+# -r : 모든 디렉토리
+# -n : 라인 수 출력
+# -i : 대소문자 구분 X
+grep -rni "hello" .
+
+# find : 이름으로 파일 또는 디렉토리 찾기
+find . -name "*.txt"
+find . -type f # 찾을 유형
+
+# find & grep 혼용
+find . -name "*.txt" | xargs grep -rni "hello"
+find . -name "*.txt" -exec grep -rni "hello" {} \;
+
+# 타입은 file인 파일 중에 이름은 first로 시작하고 그 파일을 안에 hello 라는 키워드가 들어가 있는 문장 찾기
+find . -name "first*" -type f | xargs grep -rni "hello" 
+find . -name "first*" -type f -exec grep -rni "hello" {} \;
---
+++ 3_shell_programming.sh
@@ -0,0 +1,79 @@
+# 쉘 스크립트 작성 : 반드시 확장자 .sh
+touch myscript.sh
+nano myscript.sh
+echo "hello world'"
+chmod u+x myscript.sh
+
+# 스크립트 실행
+./myscript.sh
+
+############실습############
+# 터미널 창에 "script practice start" 문구 출력
+# 홈디렉토리에서 mydir 폴더 생성
+# mydir로 이동해서 file1.txt, file2.txt 파일 생성
+# file1.txt에는 Hello from file1 문구 입력
+# file2.txt에는 Hello from file2 문구 입력
+# file1.txt파일을 백업본(copy본) 생성. 이름은 file1_backup.txt
+# file2.txt파일은 이름은 file2_rename.txt로 변경
+# 터미널창에 "script practice end" 문구 출력 
+echo "script practice start"
+mkdir ~/mydir
+cd ~/mydir
+touch file1.txt file2.txt
+chmod 777 file1.txt
+chmod 777 file2.txt
+echo "Hello from file1" > file1.txt
+echo "Hello from file2" > file2.txt
+cp file1.txt file1_backup.txt
+mv file2.txt file2_rename.txt
+echo "script practice end"
+
+# myscript2 생성 후 아래 코드 작성 및 실행 
+# if문
+if [ 1 -gt 2 ]; then
+    echo "hello world1"
+else
+    echo "hello world2"
+fi
+
+# 변수 선언 및 파일/디렉토리 검사
+file_name=first_file.txt
+if [ -f "$file_name" ]; then
+    echo "$first_name exists"
+else
+    echo "$first_name doesn't exist"
+fi
+
+# for문
+for a in {1..100}
+do
+    echo "hello world $a"
+done 
+
+# for문과 파일/디렉토리 목록조회
+for a in *
+do
+    echo "$a"
+done
+
+# for문의 변수값을 증가
+count=100
+for a in {1..100}
+do
+    let count=count+1
+done
+echo "count value is $count"
+
+# for문 및 변수값 세팅 활용 : 현재 폴더에서 파일개수와 디렉토리 개수 계산
+x=0
+y=0
+for a in * 
+do
+    if [ -f "$a" ]; then 
+        let x=x+1
+    else
+        let y=y+1
+    fi
+done
+echo "filecount is $x"
+echo "dircount is $y"
